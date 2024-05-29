648bd415990749ac3be37fc1d7b701de798a76e9
laska-adief <laskaadief.la@gmail.com>
2024-05-29 10:57:56 +0700

    [Youapp] create getAge util

---
+++ src/app/utils/getAge.ts
@@ -0,0 +1,12 @@
+export const getAge = (birthDate: string) => {
+  const today = new Date();
+  const dob = new Date(birthDate);
+  let age = today.getFullYear() - dob.getFullYear();
+  const monthDiff = today.getMonth() - dob.getMonth();
+
+  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dob.getDate())) {
+    age--;
+  }
+
+  return age;
+};
